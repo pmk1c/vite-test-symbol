ea387f7306380420600326dcf99d4e491dc9bd2a
Ruben Grimm <ruben.grimm@zweitag.de>
2021-07-03 01:24:55 +0200

    Vite test project

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+import '@jumpn/utils-composite'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
